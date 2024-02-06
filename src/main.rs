db1c9636923c46b008cf9837c5976ea552441151
Daniel Leite Gurgel do Amaral <dgurgel@petrobras.com.br>
2024-02-06 01:21:14 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,169 @@
+use std::{collections::HashMap, fs, fs::File, io::{BufRead, BufReader, Write}, path::PathBuf};
+use clap::Parser;
+use regex::Regex;
+
+/// Create docs from R scripts
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Relative path to the files
+    #[arg(short, long, num_args = 1..)]
+    files: Vec<String>,
+
+    /// Docs will be created in this path
+    #[arg(short, long)]
+    docs_path: String,
+
+    /// Source button in docs will point to files in this github url
+    #[arg(short, long, default_value_t = String::from(""))]
+    gh_url: String
+}
+
+
+fn main() {
+    let args = Args::parse();
+    let files = args.files;
+    let gh_url = args.gh_url;
+    let docs_path = PathBuf::from(args.docs_path);
+
+    println!("{:?}", files);
+    println!("{:?}", docs_path);
+
+
+    let mut hash: HashMap<String, Vec<String>> = HashMap::new();
+    generate_r_docs(files, gh_url, &mut hash);
+
+    output_file(hash, docs_path)
+}
+
+// Currently it may give a bug if 2 methods impl for the same struct are on different files,
+// depending on the order of the files on the list below. Try to reorder the vec in a way that
+// if the code chunk contains "# Methods" it will be swapped to the vec's first position.
+fn generate_r_docs(files: Vec<String>, gh_url: String, hash: &mut HashMap<String, Vec<String>>) {
+    for file in &files {
+        // Read the input file and filter to keep only lines starting with "###"
+        let input_file = File::open(file).unwrap();
+        let mut key = String::new();
+        let mut last_line_was_comment = false;
+        let mut skip_comment_chunk = false;
+
+        // counts the line in a code chunk
+        let mut counter: i32 = -1;
+        for (line_counter, line) in BufReader::new(input_file).lines().flatten().enumerate() {
+            let line_trimmed = line.trim_start();
+
+            if let Some(stripped) = line_trimmed.strip_prefix("///") {
+                counter += 1;
+                if skip_comment_chunk {
+                    continue;
+                }
+
+                // skip first space.
+                let filtered_line = stripped.strip_prefix(' ').unwrap_or(stripped).to_string();
+
+                // associate with key in first line of comment chunk. Keys are identifiable by a 1 word line.
+                if !last_line_was_comment {
+                    key = filtered_line.clone();
+                    // key should have only one word
+                    if key.contains(' ') {
+                        skip_comment_chunk = true;
+                        continue;
+                    }
+                    hash.entry(key.clone()).or_insert_with(Vec::new);
+                    last_line_was_comment = true;
+                } else {
+                    hash.get_mut(&key).unwrap().push(filtered_line);
+                }
+            } else if let Some(stripped) = line_trimmed.strip_prefix("###") {
+                counter += 1;
+                if skip_comment_chunk {
+                    continue;
+                }
+
+                // skip first space.
+                let filtered_line = stripped.strip_prefix(' ').unwrap_or(stripped).to_string();
+
+                // associate with key in first line of comment chunk. Keys are identifiable by a 1 word line.
+                if !last_line_was_comment {
+                    key = filtered_line.clone();
+                    // key should have only one word
+                    if key.contains(' ') {
+                        skip_comment_chunk = true;
+                        continue;
+                    }
+                    hash.entry(key.clone()).or_insert_with(Vec::new);
+                    last_line_was_comment = true;
+                } else {
+                    hash.get_mut(&key).unwrap().push(filtered_line);
+                }
+            } else if !gh_url.is_empty() {
+                // add the source text. Code on github must be updated.
+
+                // Regular expression to match function declarations
+                let fn_declaration_regex = Regex::new(r"\s*fn\s+[a-zA-Z_]\w*\s*\(").unwrap();
+                let function_declaration_regex = Regex::new(r"\s*function\s+[a-zA-Z_]\w*\s*\(").unwrap();
+
+                if last_line_was_comment
+                    && (fn_declaration_regex.is_match(line_trimmed) || function_declaration_regex.is_match(line_trimmed))
+                {
+                    let vec = hash.get_mut(&key).unwrap();
+                    let len = vec.len();
+                    let elem = &mut vec[len - counter as usize + 2];
+                    elem.pop();
+
+                    let filename = files
+                        .iter()
+                        .find(|&x| x.contains(&key.to_lowercase()))
+                        .unwrap();
+
+                    // Remove a possible starting dot in path.
+                    let filename_str = if filename.strip_prefix(".").is_some() {
+                        &filename[1..]
+                    } else {
+                        &filename[..]
+                    };
+
+                    let source = "<span style=\"float: right;\"> [source](".to_string()
+                        + gh_url.as_str()
+                        + filename_str
+                        + "#L"
+                        + &(line_counter + 1).to_string()
+                        + ") </span> \\";
+                    elem.push_str(&source);
+                }
+
+                counter = -1;
+                skip_comment_chunk = false;
+                last_line_was_comment = false;
+            }
+        }
+    }
+}
+
+fn output_file(hash: HashMap<String, Vec<String>>, docs_path: PathBuf) {
+    for (key, value) in hash {
+        let key_lowercase = key.to_lowercase();
+
+        // Construct the output file path as the input file path with a .qmd extension.
+        let docs_file_path = docs_path.join(&key_lowercase).with_extension("qmd");
+
+        // Create the folder if it doesn't exist.
+        fs::create_dir_all(docs_path.clone()).expect("directory could not be created");
+
+        // Write the output text to the output file.
+        write_output_file(&docs_file_path, &key, &value);
+    }
+}
+
+fn write_output_file(file_path: &PathBuf, key: &str, value: &[String]) {
+    // Construct the header
+    let title = format!("title: {}", key);
+    let text = ["---", &title, "---"].join("\n");
+
+    // Construct the final output text.
+    let output_text = [text, value.join("\n")].join("\n\n");
+
+    // Write the output text to the output file.
+    let mut output_file = File::create(file_path).expect("could not create output_file");
+    output_file.write_all(output_text.as_bytes()).expect("could not write to output_file");
+}
---
+++ src/testdir/harray.rs
@@ -0,0 +1,476 @@
+use crate::{conversions::RobjConversions, harrayr::HArrayR, hdatatype::HDataType};
+use extendr_api::prelude::*;
+use num_complex::Complex;
+use std::sync::Arc;
+
+/// HArray
+/// An array representation. \
+///
+/// # Methods
+///
+#[derive(Clone)]
+pub struct HArray(pub Arc<dyn HArrayR>);
+
+#[extendr]
+impl HArray {
+    /// HArray
+    /// ## new_from_values
+    ///
+    /// `new_from_values(arr: array, dtype: HDataType) -> HArray` \
+    ///
+    /// Creates a new `HArray` from an R array. \
+    ///
+    /// #### Arguments
+    ///
+    /// * `arr` \
+    /// A `double` or `complex` array.
+    /// * `dtype` \
+    /// An `HDataType` to indicate which type of `HArray` to be created. \
+    /// For float dtypes, the atomic vector must be a `double`. For complex dtypes, a `complex` atomic vector.
+    ///
+    /// #### Returns
+    ///
+    /// An `HArray`. \
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// HArray$new_from_values(arr, dtype)
+    /// ```
+    ///
+    /// _________
+    ///
+    fn new_from_values(arr: Robj, dtype: &HDataType) -> Robj {
+        if let Some(dim) = arr.dim() {
+            let mut dim = dim.iter().map(|z| z.inner() as usize).collect();
+            dim.reverse();
+
+            match (arr.rtype(), dtype) {
+                (Rtype::Doubles, HDataType::Float32) => {
+                    let slice: &[f64] = arr.robj_to_slice();
+                    let v: Vec<f32> = slice.iter().map(|x| *x as f32).collect();
+                    let harray = harmonium_core::array::HArray::new_from_shape_vec(dim, v).unwrap();
+                    let data = Arc::new(harray);
+                    HArray(data).into()
+                }
+                (Rtype::Doubles, HDataType::Float64) => {
+                    let v: Vec<f64> = arr.robj_to_slice().to_vec();
+                    let harray = harmonium_core::array::HArray::new_from_shape_vec(dim, v).unwrap();
+                    let data = Arc::new(harray);
+                    HArray(data).into()
+                }
+                (Rtype::Complexes, HDataType::Complex32) => {
+                    let slice: &[Rcplx] = arr.robj_to_slice();
+                    let v: Vec<Complex<f32>> = slice
+                        .iter()
+                        .map(|z| Complex::new(z.re().inner() as f32, z.im().inner() as f32))
+                        .collect();
+                    let harray = harmonium_core::array::HArray::new_from_shape_vec(dim, v).unwrap();
+                    let data = Arc::new(harray);
+                    HArray(data).into()
+                }
+                (Rtype::Complexes, HDataType::Complex64) => {
+                    let slice: &[Rcplx] = arr.robj_to_slice();
+                    let v: Vec<Complex<f64>> = slice
+                        .iter()
+                        .map(|z| Complex::new(z.re().inner(), z.im().inner()))
+                        .collect();
+                    let harray = harmonium_core::array::HArray::new_from_shape_vec(dim, v).unwrap();
+                    let data = Arc::new(harray);
+                    HArray(data).into()
+                }
+                _ => panic!("not valid input types"),
+            }
+        } else {
+            panic!("arr must be of array type.");
+        }
+    }
+
+    /// HArray
+    /// ## len
+    ///
+    /// `len() -> integer` \
+    ///
+    /// Returns the number of elements of this `Harray`. \
+    ///
+    /// #### Returns
+    ///
+    /// An `integer`. \
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray = HArray$new_from_values(arr, dtype)
+    /// harray$len()
+    /// ```
+    ///
+    /// _________
+    ///
+    fn len(&self) -> Robj {
+        let len: i32 = self.0.len().try_into().unwrap();
+        let rint = Rint::from(len);
+        rint.into()
+    }
+
+    /// HArray
+    /// ## shape
+    ///
+    /// `shape() -> integers` \
+    ///
+    /// Returns the shape of this `HArray`. \
+    ///
+    /// #### Returns
+    ///
+    /// A vector of integers. \
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray = HArray$new_from_values(arr, dtype)
+    /// harray$shape()
+    /// ```
+    ///
+    /// _________
+    ///
+    fn shape(&self) -> Robj {
+        let shape = self.0.shape();
+        let integers: Integers = shape.iter().map(|z| Rint::from(*z as i32)).collect();
+        integers.into()
+    }
+
+    /// HArray
+    /// ## ndim
+    ///
+    /// `ndim() -> integer` \
+    ///
+    /// Returns the number of dimensions of this `HArray`. \
+    ///
+    /// #### Returns
+    ///
+    /// An integer. \
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray = HArray$new_from_values(arr, dtype)
+    /// harray$ndim()
+    /// ```
+    ///
+    /// _________
+    ///
+    fn ndim(&self) -> Robj {
+        let ndim = self.0.ndim() as i32;
+        let rint = Rint::from(ndim);
+        rint.into()
+    }
+
+    /// HArray
+    /// ## slice
+    ///
+    /// `slice(range: list[atomicvector]) -> HArray` \
+    ///
+    /// Slices the HArray. \
+    /// This operation has a COW ([clone-on-write](https://doc.rust-lang.org/std/borrow/enum.Cow.html)) behaviour. The created slice shares the inner data with
+    /// the original array until one of them is modified. \
+    ///
+    /// #### Arguments
+    ///
+    /// * `range` \
+    /// A list of vectors of integers.
+    /// The number of vectors in the list must be equal to the number of dimensions in the original HArray as they represent the slice information for each axis. \
+    /// Each vector must be composed of 3 elements: [start, end, step]. All 3 values can be
+    /// positive or negative, although step can't be 0. \
+    ///
+    /// #### Returns
+    ///
+    /// An `HArray`. \
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray = HArray$new_from_values(arr, dtype)
+    /// harray$slice(list(c(0L, 2L, 1L), c(1L, 3L, 1L)))
+    /// ```
+    ///
+    /// _________
+    ///
+    fn slice(&self, range: Robj) -> HArray {
+        HArray(self.0.slice(&range))
+    }
+
+    /// HArray
+    /// ## print
+    ///
+    /// `print()` \
+    ///
+    /// Prints the `HArray`. \
+    /// Differently from R's normal behaviour, `print` doesn't return the value invisibly. \
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray = HArray$new_from_values(arr, dtype)
+    /// harray$print()
+    ///
+    /// # or similarly:
+    /// print(harray)
+    /// ```
+    ///
+    /// _________
+    ///
+    fn print(&self) {
+        self.0.print();
+    }
+
+    /// HArray
+    /// ## eq
+    ///
+    /// `eq(other: HArray) -> bool` \
+    ///
+    /// Equality with another `HArray`. \
+    /// The comparison only checks if the dtype and the values are the same. To compare if the
+    /// underlying data is the same in memory, check `mem_adress`. \
+    ///
+    /// #### Arguments
+    ///
+    /// * `other` \
+    /// An `HArray`. \
+    ///
+    /// #### Returns
+    ///
+    /// A `bool`. \
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray1 = HArray$new_from_values(arr, dtype)
+    ///
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray2 = HArray$new_from_values(arr, dtype)
+    ///
+    /// harray1$eq(harray2) # TRUE
+    ///
+    /// # or similarly:
+    /// harray1 == harray2
+    /// ```
+    ///
+    /// _________
+    ///
+    fn eq(&self, other: &HArray) -> Robj {
+        let eq = self.0.eq(&other.0);
+        let rbool = Rbool::from(eq);
+        rbool.into()
+    }
+
+    /// HArray
+    /// ## ne
+    ///
+    /// `ne(other: HArray) -> bool` \
+    ///
+    /// Difference with another `HArray`. \
+    /// The comparison only checks if the dtype and the values are the same. To compare if the
+    /// underlying data is the same in memory, check `mem_adress`.
+    ///
+    /// #### Arguments
+    ///
+    /// * `other` \
+    /// An `HArray`.
+    ///
+    /// #### Returns
+    ///
+    /// A `bool`.
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray1 = HArray$new_from_values(arr, dtype)
+    ///
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray2 = HArray$new_from_values(arr, dtype)
+    ///
+    /// harray1$ne(harray2) # FALSE
+    ///
+    /// # or similarly:
+    /// harray1 != harray2
+    /// ```
+    ///
+    /// _________
+    ///
+    fn ne(&self, other: &HArray) -> Robj {
+        let ne = self.0.ne(&other.0);
+        let rbool = Rbool::from(ne);
+        rbool.into()
+    }
+
+    /// HArray
+    /// ## clone
+    ///
+    /// `clone() -> HArray` \
+    ///
+    /// Creates a new `HArray`, with the underlying data pointing to the same place in memory.
+    ///
+    /// #### Returns
+    ///
+    /// An `HArray`.
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray1 = HArray$new_from_values(arr, dtype)
+    /// harray2 = harray1$clone()
+    /// harray1 == harray2 # TRUE
+    /// ```
+    ///
+    /// _________
+    ///
+    fn clone(&self) -> HArray {
+        std::clone::Clone::clone(self)
+    }
+
+    /// HArray
+    /// ## collect
+    ///
+    /// `collect() -> array` \
+    ///
+    /// Creates an R array from an `HArray`. The type of the array created (`double` or `complex`) will depend on the `HArray`'s dtype.
+    ///
+    /// #### Returns
+    ///
+    /// An array of type `double` or `complex`.
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray = HArray$new_from_values(arr, dtype)
+    /// harray$collect()
+    /// ```
+    ///
+    /// _________
+    ///
+    fn collect(&self) -> Robj {
+        self.0.collect()
+    }
+
+    /// HArray
+    /// ## dtype
+    ///
+    /// `dtype() -> HDataType` \
+    ///
+    /// Gets the `HArray`'s dtype as an `HDataType`.
+    ///
+    /// #### Returns
+    ///
+    /// An `HDataType`.
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray = HArray$new_from_values(arr, dtype)
+    /// harray$dtype()
+    /// ```
+    ///
+    /// _________
+    ///
+    fn dtype(&self) -> HDataType {
+        self.0.dtype()
+    }
+
+    /// HArray
+    /// ## is_shared
+    ///
+    /// `is_shared() -> bool` \
+    ///
+    /// Checks if the object is shared. \
+    /// Since `HArray` has a COW ([clone-on-write](https://doc.rust-lang.org/std/borrow/enum.Cow.html)) behaviour, this function is useful to check if a new
+    /// object will be created or if the change will be done in-place. \
+    ///
+    /// #### Returns
+    ///
+    /// A `bool`. \
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray1 = HArray$new_from_values(arr, dtype)
+    /// harray1$is_shared() # FALSE.
+    ///
+    /// harray2 = harray1$clone()
+    /// harray$is_shared() # TRUE, HArray object shared with harray2.
+    /// ```
+    ///
+    /// _________
+    ///
+    fn is_shared(&self) -> Robj {
+        let bool = Arc::weak_count(&self.0) + Arc::strong_count(&self.0) != 1;
+        let rbool = Rbool::from(bool);
+        rbool.into()
+    }
+
+    /// HArray
+    /// ## mem_adress
+    ///
+    /// `mem_adress() -> string` \
+    ///
+    /// The memory adress of the first element of the inner array. \
+    /// This is useful to check if different objects share the same underlying data. \
+    ///
+    /// #### Returns
+    ///
+    /// A `string`. \
+    ///
+    /// #### Examples
+    ///
+    /// ```r
+    /// arr = array(c(1,2,3,4,5,6,7,8,9,10,11,12), c(3,4))
+    /// dtype = HDataType$float32
+    /// harray = HArray$new_from_values(arr, dtype)
+    /// harray$mem_adress()
+    /// ```
+    ///
+    /// _________
+    ///
+    pub fn mem_adress(&self) -> String {
+        self.0.mem_adress()
+    }
+}
+
+impl HArray {
+    #[doc(hidden)]
+    pub fn get_inner_mut(&mut self) -> &mut dyn HArrayR {
+        if Arc::weak_count(&self.0) + Arc::strong_count(&self.0) != 1 {
+            self.0 = self.0.clone_inner();
+        }
+        Arc::get_mut(&mut self.0).expect("implementation error")
+    }
+}
+
+extendr_module! {
+    mod harray;
+    impl HArray;
+}
