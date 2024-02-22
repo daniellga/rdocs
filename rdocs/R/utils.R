03efd7817ecf72ac0271432da6531d00073a3f3b
Daniel Gurgel <daniellga@gmail.com>
2024-02-22 14:07:50 -0300

    quarto

--- rdocs/R/utils.R
+++ rdocs/R/utils.R
@@ -1,10 +1,10 @@
-RDOCS_URL_BASE = "https://github.com/daniellga/rdocs/releases/download"
+RDOCS_URL_BASE <- "https://github.com/daniellga/rdocs/releases/download"
 
-RDOCS_NAME = "rdocs"
+RDOCS_NAME <- "rdocs"
 
-RDOCS_CACHE_DIR = normalizePath(tools::R_user_dir("rdocs", "cache"), mustWork = FALSE)
+RDOCS_CACHE_DIR <- normalizePath(tools::R_user_dir("rdocs", "cache"), mustWork = FALSE)
 
-RDOCS_PATH = {
+RDOCS_PATH <- {
   if (Sys.info()[["sysname"]] == "Windows") {
     file.path(RDOCS_CACHE_DIR, paste0(RDOCS_NAME, ".exe"))
   } else {
@@ -12,31 +12,31 @@ RDOCS_PATH = {
   }
 }
 
-latest_release = jsonlite::read_json("https://api.github.com/repos/daniellga/rdocs/releases/latest")[["tag_name"]]
+latest_release <- jsonlite::read_json("https://api.github.com/repos/daniellga/rdocs/releases/latest")[["tag_name"]]
 
-get_download_url = function() {
-  os = Sys.info()[["sysname"]]
-  arch = Sys.info()[["machine"]]
+get_download_url <- function() {
+  os <- Sys.info()[["sysname"]]
+  arch <- Sys.info()[["machine"]]
 
-  binary = switch(os,
+  binary <- switch(os,
     Windows = "rdocs-x86_64-pc-windows-msvc.zip",
-    Linux   = if(arch == "x86-64") "rdocs-x86_64-unknown-linux-gnu.tar.xz" else "rdocs-aarch64-unknown-linux-gnu.tar.xz",
-    Darwin  = if(arch == "x86-64") "rdocs-x86_64-apple-darwin.tar.xz" else "rdocs-aarch64-apple-darwin.tar.xz"
+    Linux   = if (arch == "x86-64") "rdocs-x86_64-unknown-linux-gnu.tar.xz" else "rdocs-aarch64-unknown-linux-gnu.tar.xz",
+    Darwin  = if (arch == "x86-64") "rdocs-x86_64-apple-darwin.tar.xz" else "rdocs-aarch64-apple-darwin.tar.xz"
   )
 
   paste(RDOCS_URL_BASE, latest_release, binary, sep = "/")
 }
 
-download_rdocs = function() {
-  download_tmp_dir = tempfile()
-  extract_tmp_dir = tempfile()
+download_rdocs <- function() {
+  download_tmp_dir <- tempfile()
+  extract_tmp_dir <- tempfile()
   on.exit(unlink(download_tmp_dir, recursive = TRUE, force = TRUE), add = TRUE)
   on.exit(unlink(extract_tmp_dir, recursive = TRUE, force = TRUE), add = TRUE)
 
   # download
   dir.create(download_tmp_dir)
-  download_url = get_download_url()
-  archive_file = file.path(download_tmp_dir, basename(download_url))
+  download_url <- get_download_url()
+  archive_file <- file.path(download_tmp_dir, basename(download_url))
   download.file(download_url, destfile = archive_file, mode = "wb")
 
   # extract and copy
@@ -51,15 +51,28 @@ download_rdocs = function() {
   invisible(NULL)
 }
 
-generate_docs = function(files, docs_path = "./docs", gh_url = "") {
-  files = shQuote(normalizePath(files, mustWork = FALSE))
-  docs_path = shQuote(normalizePath(docs_path, mustWork = FALSE))
-  gh_url = shQuote(gh_url)
+generate_docs <- function(files, folder_name = "docs", gh_url = "") {
+  qmd_tmp_dir <- tempfile()
+  on.exit(unlink(qmd_tmp_dir, recursive = TRUE, force = TRUE), add = TRUE)
+
+  files <- shQuote(normalizePath(files, mustWork = FALSE))
+  html_folder <- shQuote(normalizePath(paste(folder_name, "docs", sep = "/"), mustWork = FALSE))
+  gh_url <- shQuote(gh_url)
 
   if (!file.exists(RDOCS_PATH)) {
     cat("Downloading rdocs binary")
     download_rdocs()
   }
 
-  system2(RDOCS_PATH, args = c("--files", files, "--docs-path", docs_path, "--gh-url", gh_url))
+  # Create qmd files.
+  system2(RDOCS_PATH, args = c("--files", files, "--docs-path", qmd_tmp_dir, "--gh-url", gh_url))
+  # Create quarto project if it doesn't exist.
+  if (!file.exists(folder_name)) {
+    system2("quarto", args = c("create", "project", "website", folder_name))
+  }
+  # Render qmd files to html.
+  qmd_files <- list.files(qmd_tmp_dir, full.names = TRUE)
+  for (file in qmd_files) {
+    system2("quarto", args = c("render", file, "--output-dir", html_folder))
+  }
 }
